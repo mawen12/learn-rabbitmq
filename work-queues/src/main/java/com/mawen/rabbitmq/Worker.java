d5c6d9e736a6820119ad4e8dd68fbe3c764c9a0b
马文 <1181963012mw@gmail.com>
2022-02-13 11:29:58 +0800

    六大模式 Work Queues

--- work-queues/src/main/java/com/mawen/rabbitmq/Worker.java
+++ work-queues/src/main/java/com/mawen/rabbitmq/Worker.java
@@ -16,17 +16,18 @@ public class Worker implements Runnable {
 
     @Override
     public void run() {
-        try {
-            // 获取信道
-            Channel channel = RabbitMqUtils.getChannel();
-            // 消费消息
-            channel.basicConsume(QUEUE_NAME_HELLO, AUTO_ACK, RabbitMqUtils.deliverCallback, RabbitMqUtils.cancelCallback);
-            // 此处需要等待一段事件，否则会因为未完成而提前关闭
-            Thread.sleep(100L);
-            RabbitMqUtils.close(channel, CLOSE_CONNECTION);
-        } catch (Exception e) {
-            e.printStackTrace();
+        while (true) {
+            try {
+                // 获取信道
+                Channel channel = RabbitMqUtils.getChannel();
+                // 消费消息
+                channel.basicConsume(QUEUE_NAME_HELLO, AUTO_ACK, RabbitMqUtils.deliverCallback, RabbitMqUtils.cancelCallback);
+                // 此处需要等待一段时间，否则会因为未完成而提前关闭
+                Thread.sleep(100L);
+                RabbitMqUtils.close(channel, CLOSE_CONNECTION);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
     }
 }
